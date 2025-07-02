343fda6840539827af6de50d39278ad265c8bd20
Taksh Bhatt <takshbhatt0512@gmail.com>
2025-07-03 00:19:50 +0530

    Authentication Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:memelusion/screens/auth/signup_screen.dart';
+import 'package:memelusion/screens/auth/login_screen.dart';
+import 'package:memelusion/screens/home_screen.dart';
 import 'firebase_options.dart';
-import 'screens/admin_panel.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Memelusion Admin',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const AdminPanel(),
+      title: 'Memelusion',
+      theme: ThemeData.dark(),
+      initialRoute: '/signup',
+      routes: {
+        '/signup': (context) => SignupPage(),
+        '/login': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+      },
     );
   }
 }
