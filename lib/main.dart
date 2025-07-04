57e534ccef07b5aad8f86902bb88bf57e1cb7ca0
maharshi <maharsh10302@gmail.com>
2025-07-04 14:25:00 +0530

    moj masti

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'firebase_options.dart'; // Make sure you generated this with flutterfire configure
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,29 +17,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Memelusion Home')),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () async {
+              await FirebaseFirestore.instance
+                  .collection('users')
+                  .doc('testuser')
+                  .set({
+                'username': 'Maharshi',
+                'profilePictureUrl': '',
+                'friends': [],
+              });
 
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Memelusion Home')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            await FirebaseFirestore.instance
-                .collection('users')
-                .doc('testuser')
-                .set({
-              'username': 'Test User',
-              'profilePictureUrl': '',
-              'friends': [],
-            });
-
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('Test user created in Firestore!')),
-            );
-          },
-          child: const Text('Create Test User'),
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Test user created in Firestore!')),
+              );
+            },
+            child: const Text('Create Test User'),
+          ),
         ),
       ),
     );
